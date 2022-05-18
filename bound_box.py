08dc027053a99d182329260d02f865cf79b51e7a
Shikha Singh <31015605+ss0028@users.noreply.github.com>
2022-05-18 17:09:03 +0530

    Add files via upload

---
+++ bound_box.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class BoundBox:
+    """
+    This class specifies the bounding box in an image
+    """
+
+    def __init__(self, xmin, ymin, xmax, ymax, objness=None, classes=None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+
+        self.objness = objness
+        self.classes = classes
+
+        self.label = -1
+        self.score = -1
+
+    def get_label(self):
+        if self.label == -1:
+            self.label = np.argmax(self.classes)
+
+        return self.label
+
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.get_label()]
+
+        return self.score
